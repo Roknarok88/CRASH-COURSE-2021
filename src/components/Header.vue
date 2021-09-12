90ae497818372736587056a567be9c24cef84543
Roknarok88 <afrik.gemenis@gmail.com>
2021-09-13 00:50:44 +0300

    vue app

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<header>
+    <h1>{{text}}</h1>
+    <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
+</header>
+</template>
+
+<script>
+
+import Button from "./Button.vue"
+
+export default {
+    name: 'Header',
+    components: {
+Button
+    },
+    props: {
+        text: String,
+        showAddTask: Boolean,
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :key="tasky.id" v-for="tasky in tasksprop">
+        <Task @toggle-reminder="$emit('toggle-reminder', tasky.id)" @delete-task="$emit('delete-task', tasky.id)" :task="tasky" />
+    </div>
+</template>
+
+<script>
+import Task from "./Task.vue"
+
+export default {
+    name: "Tasks",
+    components: {
+        Task,
+    },
+    props:{
+        tasksprop: Array
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
